bd9c501ffcb87fb254ddd43017e2b1faf0deec7c
MarenWoodruff <wren403@gmail.com>
2016-05-14 12:16:04 -0400

    master/details working correctly

--- app/app.component.js
+++ app/app.component.js
@@ -18,19 +18,31 @@ exports.Hero = Hero;
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
-        this.hero = {
-            id: 1,
-            name: 'Ruth Bader Ginsberg'
-        };
+        this.heroes = HEROES;
     }
+    AppComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <h1>{{title}}</h1>\n    <h2>{{hero.name}} details!</h2>\n    <div>\n      <label>id: </label>{{hero.id}}\n    </div>\n    <div>\n      <label>name: </label>\n      <input [(ngModel)]=\"hero.name\" placeholder=\"name\"> \n    </div>\n  "
+            template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n      <li *ngFor=\"let hero of heroes\" [class.selected]=\"hero === selectedHero\" (click)=\"onSelect(hero)\">\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n    <div *ngIf=\"selectedHero\">\n      <h2>{{selectedHero.name}} details!</h2>\n      <div>\n        <label>id: </label>{{selectedHero.id}}\n      </div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\"> \n      </div>\n    </div>\n  ",
+            styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .heroes {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n    .heroes li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .heroes li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .heroes li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .heroes .text {\n      position: relative;\n      top: -3px;\n    }\n    .heroes .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var HEROES = [
+    { "id": 10, "name": "Ruth Bader Ginsberg" },
+    { "id": 11, "name": "Loretta Ross" },
+    { "id": 12, "name": "Florynce Kennedy" },
+    { "id": 13, "name": "Alicia Garza" },
+    { "id": 14, "name": "Tatyana Fazlalizadeh" },
+    { "id": 15, "name": "Ai-jen Poo" },
+    { "id": 16, "name": "Gloria Anzaldúa" },
+    { "id": 17, "name": "Saru Jayaraman" },
+    { "id": 18, "name": "Charon Asetoyer" },
+    { "id": 19, "name": "Frida Kahlo" },
+    { "id": 20, "name": "Sally Ride" }
+];
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -9,21 +9,92 @@ export class Hero {
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div>
-      <label>id: </label>{{hero.id}}
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div>
+        <label>id: </label>{{selectedHero.id}}
+      </div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"> 
+      </div>
     </div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"> 
-    </div>
-  `
+  `,
+  styles:[`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
 })
 
 export class AppComponent { 
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Ruth Bader Ginsberg'
-  }
+  public heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
+
+var HEROES: Hero[] = [
+  { "id": 10, "name": "Ruth Bader Ginsberg" },
+  { "id": 11, "name": "Loretta Ross" },
+  { "id": 12, "name": "Florynce Kennedy" },
+  { "id": 13, "name": "Alicia Garza" },
+  { "id": 14, "name": "Tatyana Fazlalizadeh" },
+  { "id": 15, "name": "Ai-jen Poo" },
+  { "id": 16, "name": "Gloria Anzaldúa" },
+  { "id": 17, "name": "Saru Jayaraman" },
+  { "id": 18, "name": "Charon Asetoyer" },
+  { "id": 19, "name": "Frida Kahlo" },
+  { "id": 20, "name": "Sally Ride" }
+];
